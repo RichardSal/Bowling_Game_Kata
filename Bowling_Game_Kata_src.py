dc46ed6c137bf9037b67cae4ee4f84fc55b20412
Richard S <richard.salvara@news.co.uk>
2014-04-10 11:57:42 +0100

    First testcase. Confirm class exists

---
+++ Bowling_Game_Kata_src.py
@@ -0,0 +1,10 @@
+class game(object):
+  """docstring for game"""
+  def __init__(self):
+    pass
+
+def main():
+  pass
+
+if __name__ == '__main__':
+  main()
---
+++ Bowling_Game_Kata_tests.py
@@ -0,0 +1,13 @@
+import unittest
+import Bowling_Game_Kata_src
+
+class testCase1(unittest.TestCase):
+  """ """
+
+  def testA(self):
+    """ Class exists """
+    g = Bowling_Game_Kata_src.game()
+
+
+if __name__ == "__main__":
+  unittest.main() # run all tests
