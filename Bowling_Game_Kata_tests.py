ab51dd474244b0bf38a5b150510afe3885d2f312
Richard S <richard.salvara@news.co.uk>
2014-04-10 12:26:49 +0100

    tests: Refactored, added rollMany method to handle rolls with passed in parameters.

--- Bowling_Game_Kata_tests.py
+++ Bowling_Game_Kata_tests.py
@@ -7,16 +7,19 @@ class BowlingGameTest(unittest.TestCase):
     global g
     g = Bowling_Game_Kata_src.game()
 
+  def rollMany(self, n, pins):
+    for i in range(0, n, 1):
+      g.roll(pins)
+
   def testGutterGame(self):
     """ Class exists """
-    
-    for i in range(0, 20, 1):
-      g.roll(0)
+    self.rollMany(20, 0)
+
     self.assertEqual(0, g.getScore())
 
   def testAllOnes(self):
-    for i in range(0, 20, 1):
-      g.roll(1)
+    self.rollMany(20, 1)
+
     self.assertEqual(20, g.getScore())
 
 
