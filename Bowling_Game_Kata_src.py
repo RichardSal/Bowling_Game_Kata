61b45dd7e8b2df8ab7008c6a6d175fae6f5050b3
Richard S <richard.salvara@news.co.uk>
2014-04-11 14:33:34 +0100

    Bug fix: roll method so it can handle first index. TODO: refactor

--- Bowling_Game_Kata_src.py
+++ Bowling_Game_Kata_src.py
@@ -1,20 +1,25 @@
 class game(object):
   """docstring for game"""
   def __init__(self):
-    self.rolls = []
+    self.rolls = [0]
     self.currentRoll = 0
 
   def roll(self, pins):
+    # TODO: better way to check if first index [0], else append to end
+    if self.currentRoll == 0:
+      self.rolls[0] = pins
+    else:
+      self.rolls.append(pins)
+
     self.currentRoll += 1
-    self.rolls.append(pins)
 
   def getScore(self):
     score = 0
     i = 0
 
-    while i < len(self.rolls):
-      score += self.rolls[i]
-      i += 1
+    for frame in range(0, 10, 1):
+      score += self.rolls[i] + self.rolls[i+1]
+      i += 2
 
     return score
 
